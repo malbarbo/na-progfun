a54b400639dc45bc8e8b2a2f8f3738f52859bf35
Marco A L Barbosa <malbarbo@gmail.com>
2023-01-24 07:35:43 -0300

    tipos-de-dados: adiciona exemplo estado tarefa em python

---
+++ 04-tipos-de-dados/exemplos/estado-tarefa.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import Literal
+
+@dataclass
+class Sucesso:
+    duracao: int
+    msg: str
+
+@dataclass
+class Erro:
+    codigo: int
+    msg: str
+
+EstadoTarefa = Literal["Executando"] | Sucesso | Erro
+
+def mensagem1(estado: EstadoTarefa) -> str:
+    if isinstance(estado, str):
+        return 'A tarefa está em execução'
+    elif isinstance(estado, Sucesso):
+        return 'A tafera finalizou com sucesso ({}s): {}'.format(estado.duracao, estado.msg)
+    else:
+        return 'A tafera falhou (error {}): {}'.format(estado.codigo, estado.msg)
+
+def mensagem2(estado: EstadoTarefa) -> str:
+    match estado:
+        case str(estado):
+            return 'A tarefa está em execução'
+        case Sucesso(duracao, msg):
+            return f'A tafera finalizou com sucesso ({duracao}s): {msg}'
+        case Erro(codigo, msg):
+            return f'A tafera falhou (error {codigo}): {msg}'

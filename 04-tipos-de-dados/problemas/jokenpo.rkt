4e8abbb3d8fb8f0a4e8921beaaf440ed6f18b33d
Marco A L Barbosa <malbarbo@gmail.com>
2024-01-08 15:35:27 -0300

    tipos de dados: finaliza jokenpo.rkt

--- 04-tipos-de-dados/problemas/jokenpo.rkt
+++ 04-tipos-de-dados/problemas/jokenpo.rkt
@@ -16,9 +16,9 @@
 ;; - "tesoura"
 
 
-(struct jogada (nome mao))
-;; Representa a mao feita por um jogador
-;; nome: String - o nome do jogador
+(struct jogada (jogador mao))
+;; Representa uma jogada de um jogador no Jokenpô
+;; jogador: String - o nome do jogador
 ;; mao: Mao - o símbolo feito pelo jogador
 
 
@@ -27,3 +27,63 @@
 ;; Determina o nome do jogador, entre o jogador que fez a jogada a e o jogador
 ;; que fez a jogada b, que ganhou a rodada do Jokenpô. Se houve empate,
 ;; devolve "".
+;;
+;; Para determinar o vencedor as seguintes regras são utilizadas:
+;; - Jogada iguais empate
+;; - pedra ganha de tesoura
+;; - tesoura ganha de papel
+;; - papel ganha da pedra
+;;
+;; a / b   | pedra | papel | tesoura
+;; pedra   |   -   |   b   |  a
+;; papel   |   a   |   -   |  b
+;; tesoura |   b   |   a   |  -
+;;
+;; Requer que os nomes dos jogadores seram não vazios e diferentes.
+
+(examples
+  ; pedra x ...
+  (check-equal? (ganhador (jogada "pedro" "pedra")
+                          (jogada "paula" "pedra"))
+                "")
+  (check-equal? (ganhador (jogada "pedro" "pedra")
+                          (jogada "paula" "papel"))
+                "paula")
+  (check-equal? (ganhador (jogada "pedro" "pedra")
+                          (jogada "paula" "tesoura"))
+                "pedro")
+  ; papel x ...
+  (check-equal? (ganhador (jogada "pedro" "papel")
+                          (jogada "paula" "pedra"))
+                "pedro")
+  (check-equal? (ganhador (jogada "pedro" "papel")
+                          (jogada "paula" "papel"))
+                "")
+  (check-equal? (ganhador (jogada "pedro" "papel")
+                          (jogada "paula" "tesoura"))
+                "paula")
+  ; tesoura x ...
+  (check-equal? (ganhador (jogada "pedro" "tesoura")
+                          (jogada "paula" "pedra"))
+                "paula")
+  (check-equal? (ganhador (jogada "pedro" "tesoura")
+                          (jogada "paula" "papel"))
+                "pedro")
+  (check-equal? (ganhador (jogada "pedro" "tesoura")
+                          (jogada "paula" "tesoura"))
+                ""))
+
+(define (ganhador a b)
+  (cond
+    [(equal? (jogada-mao a) (jogada-mao b))
+     ""]
+    [(or (and (equal? (jogada-mao a) "pedra")
+              (equal? (jogada-mao b) "tesoura"))
+         (and (equal? (jogada-mao a) "papel")
+              (equal? (jogada-mao b) "pedra"))
+         (and (equal? (jogada-mao a) "tesoura")
+              (equal? (jogada-mao b) "papel")))
+     (jogada-jogador a)]
+    [else
+     (jogada-jogador b)]))
+
